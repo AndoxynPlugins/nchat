b945b0801573ed436d3e3e2fb858bb8c1885957b
Dabo Ross <dabodialga@gmail.com>
2013-06-18 17:59:15 -0700

    Implemented /me

--- src/net/daboross/bukkitdev/uberchat/commandexecutors/MeExecutor.java
+++ src/net/daboross/bukkitdev/uberchat/commandexecutors/MeExecutor.java
@@ -6,9 +6,12 @@
 package net.daboross.bukkitdev.uberchat.commandexecutors;
 
 import net.daboross.bukkitdev.uberchat.UberChatHelpers;
+import net.daboross.bukkitdev.uberchat.UberChatStatics;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -17,8 +20,12 @@ import org.bukkit.command.CommandSender;
 public class MeExecutor implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        String message = UberChatHelpers.arrayToString(args, " ");
-
+        if (args.length == 0) {
+            sender.sendMessage("Please specify an action to describe.");
+            sender.sendMessage("Example: /me says hello");
+        } else {
+            Bukkit.broadcastMessage(String.format(UberChatStatics.ME_FORMAT, sender instanceof Player ? ((Player) sender).getDisplayName() : "Server", UberChatHelpers.arrayToString(args, " ")));
+        }
         return true;
     }
 }
