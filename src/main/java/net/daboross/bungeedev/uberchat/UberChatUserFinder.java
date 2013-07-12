4bf91cb84a4734bd5507461fe43ac0d024f79b96
Dabo Ross <dabodialga@gmail.com>
2013-07-11 23:58:46 -0700

    Added General Public License to all classes. Created LICENSE.md containing the General Public License. Created LICENSE file that is packaged with the software containing the General Public License.

--- src/main/java/net/daboross/bungeedev/uberchat/UberChatUserFinder.java
+++ src/main/java/net/daboross/bungeedev/uberchat/UberChatUserFinder.java
@@ -1,16 +1,25 @@
 /*
- * Author: Dabo Ross
- * Website: www.daboross.net
- * Email: daboross@daboross.net
+ * Copyright (C) 2013 Dabo Ross <http://www.daboross.net/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.daboross.bungeedev.uberchat;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
